9ad844924fdcae87dd1b55c814bdc3db7c1dde5b
Montanher <gabrielmontanherr@gmail.com>
2024-11-07 20:34:07 -0300

    Updates and add erro404 in page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../assets/css/components/header.css';
 import imagem from '../assets/img/catiorrinho.svg';
 
 const Header = () => {
+    const [menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuAtivo(!menuAtivo);
+    };
+
+    const closeMenu = () => {
+        setMenuAtivo(false);
+    };
+
     return(
         <header className="cabecalho container">
-            <div className="menu-hamburguinho">
-                <span className="menu-hamburguinho__icone"></span>
+            <div className="menu-hamburguinho" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
             </div>
             <div className="cabecalho-container">
                 <Link to="/" className="flex flex--centro">
@@ -15,22 +27,28 @@ const Header = () => {
                     <h1 className="cabecalho__titulo">PetShop</h1>
                 </Link>
             </div>
-            <nav className="menu-cabecalho">
+
+            {/* Menu de navegação */}
+            <nav className={`menu-cabecalho ${menuAtivo ? 'menu-cabecalho--ativo' : ''}`}>
                 <ul className="menu-itens">
                     <li>
                         <Link to="/login" className="menu-item menu-item--entrar">Entrar</Link>
                     </li>
-
                     <li><Link to="/" className="menu-item">Home</Link></li>
                     <li><Link to="/product" className="menu-item">Produtos</Link></li>
                     <li><Link to="/blog" className="menu-item">Blog</Link></li>
                     <li><Link to="/about" className="menu-item">Sobre</Link></li>
                 </ul>
+                <button className="menu-cabecalho__fechar" onClick={closeMenu}></button>
             </nav>
 
-            <div className="menu-cabecalho-background"></div>
+            {/* Fundo escuro que fecha o menu */}
+            <div 
+                className={`menu-cabecalho-background ${menuAtivo ? 'menu-cabecalho-background--ativo' : ''}`} 
+                onClick={closeMenu}>
+            </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
